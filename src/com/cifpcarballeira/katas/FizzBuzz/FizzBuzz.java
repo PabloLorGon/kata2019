f82dc7c19ef2f68621e9d4db097f860e2dbd9352
Pablo Lor <pablolorenzoagrela@gmail.com>
2019-02-20 10:29:38 +0100

    1 devuelve 1

---
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz;
+
+/**
+ *
+ * @author DAM116
+ */
+public class FizzBuzz {
+
+    public String getFizzBuzz(int num) {
+        return "1";
+    }        
+    
+}
---
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz.Tests;
+
+import com.cifpcarballeira.katas.FizzBuzz.FizzBuzz;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DAM116
+ */
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+    }
+
+   
+    @Test
+     public void test1devuelve1() {
+         FizzBuzz fb = new FizzBuzz();
+         String result = fb.getFizzBuzz(1);
+         assertEquals("1", result);
+     }
+}
